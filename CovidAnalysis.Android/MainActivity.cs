590146750ccd0b5d1f98f783b802c6d7eafd9e17
StasKrivosheya <stask49@gmail.com>
2022-02-11 12:50:03 +0200

    Added OxyPlot package

--- CovidAnalysis.Android/MainActivity.cs
+++ CovidAnalysis.Android/MainActivity.cs
@@ -16,6 +16,8 @@ namespace CovidAnalysis.Droid
 
             Xamarin.Essentials.Platform.Init(this, savedInstanceState);
             global::Xamarin.Forms.Forms.Init(this, savedInstanceState);
+            OxyPlot.Xamarin.Forms.Platform.Android.PlotViewRenderer.Init();
+
             LoadApplication(new App(new AndroidInitializer()));
         }
         public override void OnRequestPermissionsResult(int requestCode, string[] permissions, [GeneratedEnum] Android.Content.PM.Permission[] grantResults)
